404f5c5894c2c063cf5ce849b5d320607457aac6
Colin Roth <colin@getwoozy.com>
2015-04-20 21:16:31 -0400

    Table View Controller implemented

---
+++ Colstagram/ImageTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImageTableViewController.h
+//  Colstagram
+//
+//  Created by Colin M. Roth on 4/20/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+@end
