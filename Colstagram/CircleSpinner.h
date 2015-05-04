7c1d27c898b777df91226f6622df1dba5f2771ee
Colin Roth <colin@getwoozy.com>
2015-05-04 07:22:38 -0400

    spinner added

---
+++ Colstagram/CircleSpinner.h
@@ -0,0 +1,17 @@
+//
+//  CircleSpinner.h
+//  
+//
+//  Created by Colin M. Roth on 5/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSpinner : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
