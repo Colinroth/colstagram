1ebfd573dfdec1abed2de74ba8dfef425e21d25a
Colin Roth <colin@getwoozy.com>
2015-07-04 20:25:35 -0400

    Filters are not loading

--- Colstagram/ImageTableViewController.m
+++ Colstagram/ImageTableViewController.m
@@ -16,6 +16,7 @@
 #import "MediaFullScreenAnimator.h"
 #import "CameraViewController.h"
 #import "ImageLibraryViewController.h"
+#import "PostToInstagramViewController.h"
 
 
 @interface ImageTableViewController () <MediaTableViewCellDelgate, UIViewControllerTransitioningDelegate, CameraViewControllerDelegate, ImageLibraryViewControllerDelegate>
@@ -106,25 +107,23 @@
     return;
 }
 
+-(void) handleImage:(UIImage *)image withNavigationController:(UINavigationController *)nav {
+    if (image) {
+        PostToInstagramViewController *postVC = [[PostToInstagramViewController alloc] initWithImage:image];
+        
+        [nav pushViewController:postVC animated:YES];
+    } else {
+        [nav dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
 -(void) cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image {
-    [cameraViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+    [self handleImage:image withNavigationController:cameraViewController.navigationController];
     
 }
 
 -(void) imageLibraryViewController:(ImageLibraryViewController *)imageLibraryViewController didCompleteWithImage:(UIImage *)image {
-    [imageLibraryViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"closed without an image.");
-        }
-    }];
+    [self handleImage:image withNavigationController:imageLibraryViewController.navigationController];
 }
 
 #pragma mark - Table view data source
@@ -411,6 +410,8 @@
         self.tableView.scrollIndicatorInsets = scrollIndicatorInsets;
     } completion:nil];
 }
+
+
 
 
 
---
+++ Colstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Colstagram
+//
+//  Created by Colin M. Roth on 7/4/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+-(instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
